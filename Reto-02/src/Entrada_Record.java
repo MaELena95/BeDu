a0cdd5f813fa16fb64f1a31a94de258cb20cc5a3
Maria Elena DDR <95491674+MaELena95@users.noreply.github.com>
2025-05-15 01:38:42 -0600

    Delete Reto-02 directory

--- Reto-02/src/Entrada_Record.java
+++
@@ -1,6 +0,0 @@
-public record Entrada_Record(String evento , double precio) {
-    public void mostrarInformacion(){
-        System.out.println("Nombre de evento" + evento + " | Precio : $ " + precio);
-
-    }
-}
